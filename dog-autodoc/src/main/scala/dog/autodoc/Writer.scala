978b8fb2f8106dd8d3b2ad067abe8106f0c74410
pocketberserker <pocketberserker@gmail.com>
2015-09-01 20:18:53 +0900

    fix compile error

--- dog-autodoc/src/main/scala/dog/autodoc/Writer.scala
+++ dog-autodoc/src/main/scala/dog/autodoc/Writer.scala
@@ -3,11 +3,12 @@ package autodoc
 
 import scalaz.{\/-, NonEmptyList}
 import scalaz.stream._
+import scalaz.concurrent.Task
 
 object Writer {
 
   def write[A](tests: Seq[(String, TestResult[Autodoc[A]])], className: String, directoryPath: String): Unit =
-    Process(tests: _*)
+    (Process(tests: _*): Process[Task, (String, TestResult[Autodoc[A]])])
       .collect { case (title, Done(NonEmptyList(\/-(x), _ @ _*))) => x.generate(title) }
       .pipe(text.utf8Encode)
       .to(io.fileChunkW(s"$directoryPath/$className.md"))
