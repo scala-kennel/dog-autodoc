03209579aee9af289e8dce1f00e8c3e1d613c6aa
pocketberserker <pocketberserker@gmail.com>
2017-05-17 08:23:53 +0900

    httpz 0.5.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object Version {
     val dog = "0.8.0"
-    val httpz = "0.5.0"
+    val httpz = "0.5.1"
   }
 
   val dogCore = "com.github.pocketberserker" %% "dog-core" % Version.dog
